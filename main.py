4f38186604b2837b12ddbd8624a08433233620b1
tycho584 <tycho.killaars@outlook.com>
2024-01-31 09:50:43 +0100

    Niet leuk dit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import pygameAddons.pygameaddons as game
+from pygameAddons.colors import Color
+
+GAME = game.AppConstructor(game.ScreenUnit.dw(100),game.ScreenUnit.dh(100), game.pygame.NOFRAME)
+GAME.centerApp()
+
+while True:
+    GAME.eventHandler(game.pygame.event.get())
+    GAME.maindisplay.fill(Color.BLUE)
+
+    GAME.updateDisplay()
