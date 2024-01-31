5692f778534635e3aca30b4628d9953fc62fa2ec
semerded <sem.van.broekhoven@gmail.com>
2024-01-31 11:25:14 +0100

    added aspectratio

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygameAddons.pygameaddons as game
 from pygameAddons.colors import Color
 
+
 GAME = game.AppConstructor(game.ScreenUnit.dw(100),game.ScreenUnit.dh(100), game.pygame.NOFRAME)
+GAME.setAspectratio(game.ScreenUnit.aspectRatio(game.aspectRatios.ratio16to9))
 GAME.centerApp()
+print(GAME.getdisplayDimensions)
 
 while True:
     GAME.eventHandler(game.pygame.event.get())
