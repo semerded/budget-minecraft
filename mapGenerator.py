b345a814f3da266c86165b43ad3e05205f0c07df
tycho584 <tycho.killaars@outlook.com>
2024-02-08 08:31:21 +0100

    pictures made

--- mapGenerator.py
+++ mapGenerator.py
@@ -1,1 +1,15 @@
 import random
+
+map = []
+mapLayer = []
+
+height = 128
+width = 2560
+
+for heightLayer in range(height):
+    for widthLayer in range(width):
+        mapLayer.append(random.randint())
+    
+    map.append(mapLayer)
+
+print(map)
