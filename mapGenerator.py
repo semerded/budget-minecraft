2f20cb7f925d59191d5a599bed95aee091144eaf
tycho584 <tycho.killaars@outlook.com>
2024-02-28 10:46:54 +0100

    Mapgeneratie aangepast naar magische bomen (1 stuk hout) + spawn karakter gebracht naar instant op de grond

--- mapGenerator.py
+++ mapGenerator.py
@@ -12,14 +12,28 @@ with open("block/blockList.json") as fp:
 for heightLayer in range(height):
     mapLayer = []
     for widthLayer in range(width):
-        if heightLayer <= 130:
+        if heightLayer <= 129:
             mapLayer.append(mapdata[0]["id"])
+
+        elif heightLayer == 130:
+            chanceOnTree = random.randint(0,20)
+            if chanceOnTree == 1:
+                mapLayer.append(mapdata[5]["id"])
+            else:
+                mapLayer.append(mapdata[0]["id"])
 
         elif heightLayer == 131:
             mapLayer.append(mapdata[1]['id'])
 
         elif heightLayer >= 132 and heightLayer <= 140:
             mapLayer.append(mapdata[2]['id'])
+
+        elif heightLayer > 140 and heightLayer < 253:
+            chanceOnRandomOre = random.randint(0,74)
+            if chanceOnRandomOre == 0:
+                mapLayer.append(mapdata[random.randint(16,25)]["id"])
+            else:
+                mapLayer.append(mapdata[8]["id"])
 
         elif heightLayer >= 246 and heightLayer < 253: 
             chanceOnObamium = random.randint(0,499)
@@ -31,13 +45,6 @@ for heightLayer in range(height):
                     mapLayer.append(mapdata[random.randint(16,25)]["id"])
                 else:
                     mapLayer.append(mapdata[8]["id"])
-
-        elif heightLayer > 140 and heightLayer < 253:
-            chanceOnRandomOre = random.randint(0,74)
-            if chanceOnRandomOre == 0:
-                mapLayer.append(mapdata[random.randint(16,25)]["id"])
-            else:
-                mapLayer.append(mapdata[8]["id"])
     
         elif heightLayer == 253:
             if random.randint(0,4) == 2:
@@ -56,6 +63,13 @@ for heightLayer in range(height):
 
         elif heightLayer > 256:
             mapLayer.append(mapdata[0]["id"])
+
+        if mapdata[5]["id"] in mapLayer:
+            if mapLayer[-2] == mapdata[5]["id"]:
+                mapLayer[-1] = mapdata[0]["id"]
+
+            else:
+                map[heightLayer -1][widthLayer] == mapdata[5]["id"]
     
     map.append(mapLayer)
     # print(map)
